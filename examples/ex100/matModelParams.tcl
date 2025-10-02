08f65ee559f51b2105c6c0c9f99321a43374d4af
yannRogerPhD <yannrogerfeutseu@gmail.com>
2025-10-02 10:44:34 -0400

    ex100 - matParams update1

---
+++ examples/ex100/matModelParams.tcl
@@ -0,0 +1,15 @@
+# parameters needed to define the P-I-M-Y material
+set gammaPeak 0.05
+set phi 0.0
+set pressCoef 0.0
+
+set nd 2
+set rho $rhoS
+set refShearModul $G
+set refBulkModul $K
+set cohesion 95.0
+set peakShearStra $gammaPeak
+set frictionAng $phi
+set refPress 80.0
+set pressDependCoe $pressCoef
+set noYieldSurf 20
